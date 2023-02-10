42803fbaadfd9a6c8a5ac5cb27e41d2fd9b345a5
Jeff Hammer <jeffrey.hammer2019@gmail.com>
2023-02-10 12:54:15 -0600

    added fetch in public index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,23 @@
-// functionality for front end 
+var vehicleList = document.querySelector('ul');
+var fetchButton = document.getElementById('fetch-button');
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '495e45f322msh4fca825b3c3afa3p19ec03jsnd3b134bddfbd',
+		'X-RapidAPI-Host': 'car-data.p.rapidapi.com'
+	}
+};
+fetch('https://car-data.p.rapidapi.com/cars?limit=10&page=0', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
+
+      for (var i = 0; i < 10; i++) {
+        var listItem = document.createElement('li');
+        listItem.textContent = data[i].html_url;
+        repoList.appendChild(listItem);
+      }
+
+
+fetchButton.addEventListener('click', getApi);
