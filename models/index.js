8e308d57c80ca91fd2d90c6271fffb4c686d0d01
Karen Peazzoni <119969744+kpeazzoni@users.noreply.github.com>
2023-02-11 20:33:41 -0500

    fixed bugs, got user login and logout working, and sign up working with saving to db

--- models/index.js
+++ models/index.js
@@ -2,14 +2,9 @@
 const User = require('./User')
 const Car = require('./Car')
 
-// User.hasMany(Car, {
-//     foreignKey: 'user_id'
-// }),
 
-// Car.belongsTo(User, {
-//     foreignKey: 'user_id'
-// }),
-
-
+Car.belongsTo(User, {
+    foreignKey: 'user_id'
+}),
 
 module.exports = { User, Car }
