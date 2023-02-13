b43dbc5c364c9257a6fecfde00ac5fc76a6ddedf
Karen Peazzoni <119969744+kpeazzoni@users.noreply.github.com>
2023-02-13 14:32:46 -0500

    added a save button

--- controllers/search-routes.js
+++ controllers/search-routes.js
@@ -3,8 +3,27 @@ const router = require('express').Router();
 const { Car} = require('../models');
 const withAuth = require('../utils/Auth'); //makes user sign back in if session is expired
 
-router.get('/', withAuth, async (req, res) => {
-  res.render('profile')
-})
+// router.get('/', withAuth, async (req, res) => {
+//   res.render('profile')
+// })
+
+router.get('/profile', async (req, res) => {
+  const userId = req.session.user_id;
+  const user = await User.findOne({
+      where: { id: userId },
+      attributes: { exclude: ['password']}
+  });
+  const userData = user.get({ plain: true });
+console.log(userData);
+
+  res.render('profile', {
+      ...userData,
+      logged_in: req.session.logged_in,
+  }
+);
+});
+
+module.exports = router;
+
 
 module.exports = router;
