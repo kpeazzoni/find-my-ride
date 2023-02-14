82013dc461c034ca73c6aae6c824e9269f221708
Jeff Hammer <jeffrey.hammer2019@gmail.com>
2023-02-14 10:49:43 -0600

    cleaned up unused code and psuedo

--- controllers/search-routes.js
+++ controllers/search-routes.js
@@ -3,9 +3,6 @@ const router = require('express').Router();
 const { Car} = require('../models');
 const withAuth = require('../utils/Auth'); //makes user sign back in if session is expired
 
-// router.get('/', withAuth, async (req, res) => {
-//   res.render('profile')
-// })
 
 router.get('/profile', async (req, res) => {
   const userId = req.session.user_id;
