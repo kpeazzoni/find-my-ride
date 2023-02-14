d9d76a308746acf9ca31b2fab48c62c76e3e7556
Karen Peazzoni <119969744+kpeazzoni@users.noreply.github.com>
2023-02-14 11:30:04 -0500

    added a password reveal

--- public/js/login.js
+++ public/js/login.js
@@ -1,8 +1,9 @@
+
 const loginFormHandler = async (event) => {
   event.preventDefault();
   // Collect values from the login form
   const email = document.querySelector('#email-login').value;
-  const password = document.querySelector('#password-login').value;
+  const password = document.querySelector('#typepass').value;
 
   if (email && password) {
     // Send a POST request to the API endpoint
