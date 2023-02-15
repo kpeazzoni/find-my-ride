5103f60ac064bf5eef58cdb109d19d47349c2215
jacob m <jacobamaynard@gmail.com>
2023-02-14 20:01:10 -0600

    changes to css on search page

--- public/js/profile.js
+++ public/js/profile.js
@@ -54,12 +54,13 @@ const carDataSearchHandler = async (event) => {
     for (var i = 0; i < json.length; i++) {
         let carWrapper = document.createElement('div');
         carWrapper.className = "car-item";
-        carWrapper.style.cssText = 'border-radius: 25px;border: 2px solid black;padding: 20px;margin-bottom: 20px;width: fit-content;background-color: #D9D9D9;box-shadow: 10px 5px 5px #353535;';
+        carWrapper.style.cssText = 'border-radius: 15px;border: 2px solid black;padding: 20px;margin-bottom: 20px;width: fit-content;background-color: #D9D9D9;box-shadow: 10px 5px 5px #353535;display: inline-block; margin: 20px 20px;';
 
         let carMake = document.createElement('h1');
         let carYear = document.createElement('h2');
         let carModel = document.createElement('h2');
         let btn = document.createElement("BUTTON");
+        btn.style.cssText = 'position: relative;display: block;outline: none;border: none;cursor: pointer;background: #3C6E71;padding-top: 0px;margin: 0 auto;font-size: 16px;color: #fff;border-radius: 10px;letter-spacing: 1.5px;box-shadow: 0 6px #353535;height: 20px;margin-top: 20px;'
         btn.setAttribute('id', 'save-button');
         btn.setAttribute('data-make', json[i].make)
         btn.setAttribute('data-model', json[i].model)
